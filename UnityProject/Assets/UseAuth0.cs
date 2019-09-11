e1e98a1e562f059ff6dca6a60548dee057e96f05
Mike OShea <oshea00@gmail.com>
2019-09-10 17:35:51 -0700

    resurrected missing script

---
+++ UnityProject/Assets/UseAuth0.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseAuth0 : MonoBehaviour
+{
+    public InputField message;
+    public Text userName;
+    public Text userEmail;
+    public Text userId;
+
+    private AndroidJavaClass auth0Helper;
+    private AndroidJavaClass player;
+
+    private void Start()
+    {
+        auth0Helper = new AndroidJavaClass("com.limpidfox.unityplugin.UnityAuth0Helper");
+        player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        auth0Helper.CallStatic("init",
+            "YOUR_CALLBACK_SCHEME",
+            player.GetStatic<AndroidJavaObject>("currentActivity")
+        );
+
+    }
+
+    public void Login()
+    {
+        auth0Helper.CallStatic("login", "YOUR_AUTH0_API_AUDIENCE");
+    }
+
+    public void Logout()
+    {
+        auth0Helper.CallStatic("logout");
+    }
+
+    public void Call()
+    {
+        message.text = auth0Helper.GetStatic<string>("returnValue");
+        userEmail.text = auth0Helper.GetStatic<string>("userEmail");
+        userName.text = auth0Helper.GetStatic<string>("userName");
+        userId.text = auth0Helper.GetStatic<string>("userId");
+    }
+}
